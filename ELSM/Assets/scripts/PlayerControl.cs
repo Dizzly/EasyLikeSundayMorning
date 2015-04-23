292b16a40c0b0657e314e5ed2d731b2a5ac1ea42
Aldo <aldocurtis@gmail.com>
2015-04-23 21:47:41 +0100

    Simple character controller

---
+++ ELSM/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+
+	public float speed_ =8;
+	public float backSpeed_=3;
+	public float rotSpeed_=0.4f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("w")) {
+			Vector3 dir=transform.forward;
+			GetComponent<Rigidbody>().AddForce(dir*speed_);
+		}
+		if (Input.GetKey ("a")) {
+			GetComponent<Rigidbody>().AddTorque(-transform.up*rotSpeed_);
+		}
+		if (Input.GetKey ("d")) {
+			GetComponent<Rigidbody>().AddTorque(transform.up*rotSpeed_);
+		}
+		if (Input.GetKey("s")) {
+			Vector3 dir=transform.forward;
+			GetComponent<Rigidbody>().AddForce(-dir*backSpeed_);
+		}
+
+
+	}
+}
