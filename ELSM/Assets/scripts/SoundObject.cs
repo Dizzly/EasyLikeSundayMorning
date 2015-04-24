d636a29aa9d1bb7861085ae57a84e3be8522a676
Aldo <aldocurtis@gmail.com>
2015-04-24 16:42:07 +0100

    Simple audio wheel system

---
+++ ELSM/Assets/scripts/SoundObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundObject : MonoBehaviour {
+
+	//the percived loudness of the object as percived by the noise wheel
+	public float priority;
+	// a representation of logarithmic falloff
+	public float soundFalloff;
+
+
+	//sound prefab, do not use in code!
+	public GameObject displayIconPrefab;
+	[HideInInspector]
+	public GameObject displayIcon;
+
+	// Use this for initialization
+	void Start () {
+		displayIcon = GameObject.Instantiate (displayIconPrefab);
+		displayIcon.SetActive (false);
+		SoundWheel s = GameObject.FindGameObjectWithTag ("Player").GetComponent<SoundWheel> ();
+		s.AddSoundObject (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnDisable()
+	{
+		RemoveFromSoundWheel ();
+	}
+
+	void OnDestroy()
+	{
+		RemoveFromSoundWheel ();
+
+	}
+
+	void RemoveFromSoundWheel()
+	{
+		SoundWheel s=GameObject.FindGameObjectWithTag ("Player").GetComponent<SoundWheel> ();
+		s.RemoveSoundObject (this.gameObject);
+	}
+
+}
---
+++ ELSM/Assets/scripts/SoundWheel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SoundWheel : MonoBehaviour {
+	
+	public List<GameObject> soundObjects;
+
+	public float displayRadius=2;
+	public float checkRadius=10;
+
+	public float maxIconScale;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void AddSoundObject(GameObject s)
+	{
+		soundObjects.Add(s);
+		s.GetComponent<SoundObject> ().displayIcon.SetActive (true);
+	}
+	public void RemoveSoundObject(GameObject s)
+	{
+		soundObjects.Remove (s);
+		s.GetComponent<SoundObject> ().displayIcon.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for(int i=0;i<soundObjects.Count;++i)
+		{
+			Vector3 dir= ((GameObject)soundObjects[i]).transform.position-  this.transform.position;
+			float dirLen=dir.magnitude;
+			dir.Normalize();
+			SoundObject s=((GameObject)soundObjects[i]).GetComponent<SoundObject>();
+			Vector3 pos= transform.position+dir*displayRadius;
+			s.displayIcon.transform.position=pos;
+			Vector3 scale=new Vector3(1,1,1)*s.priority;
+			scale/=(dirLen*s.soundFalloff);
+			s.displayIcon.transform.localScale= scale;
+
+		}
+	}
+}
